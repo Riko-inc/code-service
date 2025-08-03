c21ac772b44901d3b9eb6d82ac40c13c1f119205
Maxim <therikomero@gmail.com>
2025-08-03 22:19:22 +0300

    init commit

---
+++ src/test/java/com/example/codeservice/CodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
